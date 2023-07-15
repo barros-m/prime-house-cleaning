59cfc2af52e9f57674f3632d69d69227c7745b0d
Matheus Barros <matheusbarros1305@gmail.com>
2023-07-15 08:47:19 -0400

    Fix navbar width

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,13 +5,15 @@ import logo from '../../assets/images/logos/logo-no-background-title-only.png';
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-logo">
-        <img src={logo} alt="Logo" />
-      </div>
-      <ul className="navbar-menu">
-        <li className="menu-item">Services</li>
-        <li className="menu-item">About Us</li>
-      </ul>
+        <div className='navbar-content'>
+            <div className="navbar-logo">
+                <img src={logo} alt="Logo" />
+            </div>
+            <ul className="navbar-menu">
+                <li className="menu-item">About Us</li>
+                <li className="menu-item">Services</li>
+            </ul>
+        </div>
     </nav>
   );
 };
