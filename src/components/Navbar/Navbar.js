4316d98a54267a19ae3c01dbf05a2f58ba7fe098
Matheus Barros <matheusbarros1305@gmail.com>
2023-07-15 12:03:46 -0400

    Fix scrolling on menu item click (padding)

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './Navbar.css';
 import logo from '../../assets/images/logos/logo-no-background-title-only.png';
 
 const Navbar = () => {
-
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  const navbarRef = useRef(null);
 
   const toggleMobileMenu = () => {
     setIsMobile(!isMobile);
@@ -13,7 +13,9 @@ const Navbar = () => {
   const scrollToSection = (sectionId) => {
     const section = document.getElementById(sectionId);
     if (section) {
-      section.scrollIntoView({ behavior: 'smooth' });
+      const navbarHeight = navbarRef.current.offsetHeight;
+      const sectionPosition = section.offsetTop - navbarHeight;
+      window.scrollTo({ top: sectionPosition, behavior: 'smooth' });
       if (isMobile) toggleMobileMenu();
     }
   };
@@ -30,18 +32,22 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className={`navbar ${isMobile ? 'mobile' : ''}`}>
-        <div className='navbar-content'>
-            <div className="navbar-logo">
-                <img src={logo} alt="Logo" />
-            </div>
-            {!isMobile && (
-              <ul className="navbar-menu">
-                  <li className="menu-item" onClick={() => scrollToSection('about-us')}>About Us</li>
-                  <li className="menu-item" onClick={() => scrollToSection('our-services')}>Services</li>
-              </ul>
-            )}
+    <nav ref={navbarRef} className={`navbar ${isMobile ? 'mobile' : ''}`}>
+      <div className="navbar-content">
+        <div className="navbar-logo">
+          <img src={logo} alt="Logo" />
         </div>
+        {!isMobile && (
+          <ul className="navbar-menu">
+            <li className="menu-item" onClick={() => scrollToSection('about-us')}>
+              About Us
+            </li>
+            <li className="menu-item" onClick={() => scrollToSection('our-services')}>
+              Services
+            </li>
+          </ul>
+        )}
+      </div>
     </nav>
   );
 };
