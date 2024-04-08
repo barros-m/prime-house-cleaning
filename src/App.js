1696b2a15c8aeb619a3617857f85ca0a50927950
Matheus Barros <matheusbarros1305@gmail.com>
2024-04-07 21:27:26 -0400

    Changed whatsapp button to be a SMS button

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import OurServices from "./components/OurServices/OurServices";
 import './assets/fonts/fonts.css';
-import { FaWhatsapp } from 'react-icons/fa';
+import { FiMessageSquare } from "react-icons/fi";
 
 function App() {
     // Get cleaning images from here:  https://unsplash.com/s/photos/cleaning
     // and check this website: https://my.sites.logo.com/preview/6d8cd7005c5e49b689e45cc4c9329570?templateId=1003737&languageCode=en&_gl=1*91hl2q*_gcl_au*MTQ1MTMzNTUyOC4xNjg4NzgwMTY2*_ga*NTg0MjA0MDguMTY4ODc4MDE2Ng..*_ga_8Y782WJ29W*MTY4ODc4MDE2Ni4xLjEuMTY4ODc4MTk1Ny41MC4wLjA.
     
-    const handleWhatsAppClick = () => {
-        // Implement your logic here to open the WhatsApp chat
-        // For example, you can use window.open() with the WhatsApp URL
-        window.open('https://api.whatsapp.com/send?phone=5616746458', '_blank');
-      };
+    const handleSmsClick = () => {
+        window.open('sms:+15616746458?body=Hi%2C%20I%E2%80%99m%20interested%20in%20learning%20more%20about%20your%20cleaning%20services%20and%20getting%20a%20free%20estimate.%20Thank%20you%21', '_blank');
+    };
+    
+    
 
     return (
         <div className="App">
@@ -28,8 +28,8 @@ function App() {
             </div>
             
             <Footer />
-            <button className="whatsapp-button" onClick={handleWhatsAppClick}>
-                <FaWhatsapp />
+            <button className="whatsapp-button" onClick={handleSmsClick}>
+                <FiMessageSquare />
             </button>
         </div>
     );
